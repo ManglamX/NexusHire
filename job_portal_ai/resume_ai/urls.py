302047ad7c296b2f2b34edb354730c869f97e8d8
ManglamX <ecs.crce.10127@gmail.com>
2026-03-25 14:10:09 +0530

    removed unsual files

--- job_portal_ai/resume_ai/urls.py
+++
@@ -1,9 +0,0 @@
-from django.urls import path
-from .views import JobMatchView, UploadResumeView, CompareCandidatesView
-
-urlpatterns = [
-    path('upload/', UploadResumeView.as_view(), name='upload_resume'),
-    path('compare/', CompareCandidatesView.as_view(), name='compare_candidates'),
-    path('match/', JobMatchView.as_view()),
-    
-]
